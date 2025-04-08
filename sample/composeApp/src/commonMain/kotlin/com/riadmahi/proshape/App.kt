6d4b3bc109a192a131f8d001043ad9fe56be51e2
Riad Mahi <riad.mahi@remedee.com>
2025-04-08 23:19:17 +0200

    Call ProShapeShowcase in sample app

--- sample/composeApp/src/commonMain/kotlin/com/riadmahi/proshape/App.kt
+++ sample/composeApp/src/commonMain/kotlin/com/riadmahi/proshape/App.kt
@@ -1,15 +1,7 @@
 package com.riadmahi.proshape
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
@@ -17,11 +9,6 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-        }
+        ProShapeShowcase()
     }
 }
