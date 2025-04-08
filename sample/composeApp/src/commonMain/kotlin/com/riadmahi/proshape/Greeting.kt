5260109d011404c84fd468aa14415c6c1c814fc2
Riad Mahi <riad.mahi@remedee.com>
2025-04-08 22:39:55 +0200

    Remove platform code from sample

--- sample/composeApp/src/commonMain/kotlin/com/riadmahi/proshape/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.riadmahi.proshape
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- sample/composeApp/src/commonMain/kotlin/com/riadmahi/proshape/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.riadmahi.proshape
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
